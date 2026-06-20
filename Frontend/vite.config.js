9a3082e7f3a7ee9f42b782920644c9f0864368c8
Mithun Kumar Yadav <mithunkumaryaduvansy123@gmail.com>
2026-06-20 11:54:05 +0530

    some fix to deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      "/api" :"ai-interview-question-practise.vercel.app"
+    }
+  }
 })
