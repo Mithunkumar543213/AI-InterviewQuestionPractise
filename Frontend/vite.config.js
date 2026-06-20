040c107a174d80424b7d79446ba80780a4fd39fa
Mithun Kumar Yadav <mithunkumaryaduvansy123@gmail.com>
2026-06-20 13:15:51 +0530

    add backend path

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      "/api" :"ai-interview-question-practise.vercel.app"
+      "/api" :"https://ai-interviewquestionpra.onrender.com"
     }
   }
 })
